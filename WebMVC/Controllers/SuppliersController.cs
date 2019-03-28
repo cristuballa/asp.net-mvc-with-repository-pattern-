c6d411dcbb82e7a625d26488978f3ec7555adb22
Cris Tuballa <cristuballa@gmail.com>
2019-03-28 22:30:15 +1300

    Added supplier crud

---
+++ WebMVC/Controllers/SuppliersController.cs
@@ -0,0 +1,124 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ApplicationCore.Entities;
+using Infrastructure.Data;
+
+namespace WebMVC.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private InventoryDBContext db = new InventoryDBContext();
+
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            return View(db.Supplier.ToList());
+        }
+
+        // GET: Suppliers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Supplier.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suppliers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CompanyName,ContactPerson,EmailAdd,ContactInfo")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Supplier.Add(supplier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Supplier.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CompanyName,ContactPerson,EmailAdd,ContactInfo")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supplier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Supplier.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Supplier supplier = db.Supplier.Find(id);
+            db.Supplier.Remove(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
